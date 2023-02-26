fcbee686ea34e29d0128b8d65c363d9daae1cbf1
Orlan29 <bricebouadou@gmail.com>
2023-02-26 11:20:53 +0000

    Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
+import { httpAuthInterceptor } from './interceptors';
 
 @NgModule({
   declarations: [
@@ -24,8 +25,14 @@ import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
     SingleFaceSnapComponent,
     NewFaceSnapComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
-  providers: [{ provide: LOCALE_ID, useValue: 'fr-CI' }],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-CI' }, httpAuthInterceptor],
   bootstrap: [AppComponent],
 })
 export class AppModule {
